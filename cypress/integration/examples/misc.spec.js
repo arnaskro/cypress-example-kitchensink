b5fefc5ebc18d3a3ea46a334e16f62bffb117d1c
Arnas Kromelis <arnas.kromelis@gmail.com>
2021-02-12 12:37:37 +0100

    Update misc.spec.js

--- cypress/integration/examples/misc.spec.js
+++ cypress/integration/examples/misc.spec.js
@@ -78,21 +78,22 @@ context('Misc', () => {
   context('Cypress.Screenshot', function () {
     it('cy.screenshot() - take a screenshot', () => {
       // https://on.cypress.io/screenshot
-      cy.screenshot('my-image')
+      //cy.screenshot('my-image')
+      cy.matchImageSnapshot()
     })
 
-    it('Cypress.Screenshot.defaults() - change default config of screenshots', function () {
-      Cypress.Screenshot.defaults({
-        blackout: ['.foo'],
-        capture: 'viewport',
-        clip: { x: 0, y: 0, width: 200, height: 200 },
-        scale: false,
-        disableTimersAndAnimations: true,
-        screenshotOnRunFailure: true,
-        onBeforeScreenshot () { },
-        onAfterScreenshot () { },
-      })
-    })
+//     it('Cypress.Screenshot.defaults() - change default config of screenshots', function () {
+//       Cypress.Screenshot.defaults({
+//         blackout: ['.foo'],
+//         capture: 'viewport',
+//         clip: { x: 0, y: 0, width: 200, height: 200 },
+//         scale: false,
+//         disableTimersAndAnimations: true,
+//         screenshotOnRunFailure: true,
+//         onBeforeScreenshot () { },
+//         onAfterScreenshot () { },
+//       })
+//     })
   })
 
   it('cy.wrap() - wrap an object', () => {
